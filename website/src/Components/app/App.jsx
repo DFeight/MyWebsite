df7619d03fd37f680388e295c75db2b4edee4168
unknown <dakotalfeight@gmail.com>
2019-07-03 21:15:29 -0500

    Added CSS Grid for layout of the page, created sidebar component

--- website/src/Components/app/App.jsx
+++ website/src/Components/app/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Sidebar from '../sidebar/Sidebar';
+
+import './App.scss';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="grid">
+      <Sidebar />
+      <div className="interactive">Interactive</div>
+      <div className="about">About</div>
+    </div>
+  );
 }
 
 export default App;
