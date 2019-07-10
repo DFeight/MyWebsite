6cb438419377dd0f6cac556334a20a2c4d900610
unknown <dakotalfeight@gmail.com>
2019-07-09 20:33:14 -0500

    Finalized layout, setup border css for future interaction with panels

--- website/src/Components/app/App.jsx
+++ website/src/Components/app/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Sidebar from '../sidebar/Sidebar';
 
 import './App.scss';
 
 function App() {
   return (
     <div className="grid">
-      <Sidebar />
+      <div className="sidebar">Sidebar</div>
       <div className="interactive">Interactive</div>
       <div className="about">About</div>
     </div>
