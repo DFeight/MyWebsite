3efc7bd1b82a1cd710b47f8fbeee270da59768df
unknown <dakotalfeight@gmail.com>
2019-07-14 20:43:03 -0500

    Updated sidebar with social media icons and image

--- website/src/Components/app/App.jsx
+++ website/src/Components/app/App.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import Terminal from 'terminal-in-react';
+import { MdPictureAsPdf } from 'react-icons/md';
+import { IoIosMail, IoLogoLinkedin, IoLogoGithub } from 'react-icons/io';
 import Modal from '../modal/Modal';
+import Pdf from '../../documents/Resume.pdf';
+import Portrait from '../../images/PortraitPic.png';
 
 import './App.scss';
 
 function App() {
+  const linkedin = 'https://www.linkedin.com/in/dakotafeight/';
+  const github = 'https://github.com/DFeight';
+
   const [isOpen, setIsOpen] = useState(false);
   const [website, setWebsite] = useState('');
 
@@ -15,7 +22,41 @@ function App() {
 
   return (
     <div className="grid">
-      <div className="sidebar" />
+      <div className="sidebar">
+        <h1>Dakota Feight</h1>
+        <div className="portrait">
+          <img src={Portrait} alt="" />
+        </div>
+        <div className="contactInfo">
+          <IoIosMail
+            title="Email"
+            size="2em"
+            color="#29ff62"
+            className="icons"
+          />
+          <IoLogoLinkedin
+            title="LinkedIn"
+            size="2em"
+            color="#1399ff"
+            className="icons"
+            onClick={() => openModal(linkedin)}
+          />
+          <IoLogoGithub
+            title="GitHub"
+            size="2em"
+            color="#000203"
+            className="icons"
+            onClick={() => openModal(github)}
+          />
+          <MdPictureAsPdf
+            title="Resume"
+            size="2em"
+            color="#ff0634"
+            className="icons"
+            onClick={() => openModal(Pdf)}
+          />
+        </div>
+      </div>
       <div className="interactive">
         <Terminal
           msg="Welcome! Enter the command 'help' to get a list of available commands."
@@ -25,9 +66,8 @@ function App() {
           prompt="#26EA20"
           startState="maximised"
           commands={{
-            linkedin: () =>
-              openModal('https://www.linkedin.com/in/dakotafeight/'),
-            github: () => openModal('https://github.com/DFeight')
+            linkedin: () => openModal(linkedin),
+            github: () => openModal(github)
           }}
           descriptions={{
             linkedin: 'Visit my LinkedIn profile',
@@ -39,7 +79,9 @@ function App() {
         />
         {isOpen && <Modal website={website} />}
       </div>
-      <div className="about">About</div>
+      <div className="about">
+        <button id="btnProjects">Projects</button>
+      </div>
     </div>
   );
 }
